e627fd798253f3c7c69110992e1df2248af8f18e
Quera <kevin.duranty@quera.fr>
2025-04-09 08:11:02 +0200

    Deep-QLearning

--- Q-learning.py
+++ Q-learning.py
@@ -1,6 +1,10 @@
 from agent import *
 from environement import *
-from training import epsilon_greedy_choice
+from training import *
+import numpy as np
+import pandas as pd
+import random
+import time
 
 # --- Interface Streamlit ---
 st.set_page_config(layout="centered")
@@ -8,17 +12,40 @@ st.title("Q-Learning - Agent in GridWorld")
 
 col1, col2, col3 = st.columns(3)
 
+# Position du tableau
+placeholder = st.empty()
+
+
 with col1 : alpha = st.slider("Learning rate", 0.0, 1.0, 0.50)
 with col2 : gama = st.slider("gama", 0.0, 1.0, 0.20)
 with col3 : epsilon = st.slider("epsilon", 0.0, 1.0, 0.4)
 
+if st.sidebar.checkbox("Set up grid size"):
 
-# Position du tableau
-placeholder = st.empty()
+    try:
+        del st.session_state.Q, gw, agent, st.session_state.agent_pos
+    except:
+        pass
+    width = st.sidebar.number_input("Width", 1, 10, 4)
+    height = st.sidebar.number_input("Height", 1, 10, 3)
+    goal_pos_x = st.sidebar.number_input("Goal position x", 0, width-1, 3)
+    goal_pos_y = st.sidebar.number_input("Goal position y", 0, height-1, 2)
+    wall_pos_x = st.sidebar.number_input("Wall position x", 0, width-1, 1)
+    wall_pos_y = st.sidebar.number_input("Wall position y", 0, height-1, 1)
+    danger_pos_x = st.sidebar.number_input("Danger position x", 0, width-1, 3)
+    danger_pos_y = st.sidebar.number_input("Danger position y", 0, height-1, 1)
+    gw = GridWorld(width=width, height=height, 
+                   goal_pos=(goal_pos_x, goal_pos_y),
+                   danger_pos=(danger_pos_x, danger_pos_y),
+                     wall_pos=(wall_pos_x, wall_pos_y))
+    agent = Agent(0, 0, gw, placeholder)
+else:
+    # Initialisation de la grille par défaut
+    gw = GridWorld()
+    agent = Agent(0, 0, gw, placeholder)
+    # Initialisation de la position de l'agent
 
-# Création de l'agent et du tableau
-gw = GridWorld()
-agent = Agent(0, 0, gw, placeholder)
+
 
 
 # Boutons pour déplacer l'agent
@@ -42,7 +69,16 @@ if st.sidebar.button("Reset all values"):
 
 sleep_time = st.sidebar.slider("Speed step in second", 0.0, 2.0, 0.5)
 n_episodes = st.sidebar.slider("N episode", 0, 20, 10)
+dict_actions = {
+                0:'up', 
+                1:'down', 
+                2:'left', 
+                3:'right'
+            }
 
+def get_state():
+    x, y = st.session_state.agent_pos
+    return np.array([x / (gw.GRID_WIDTH-1), y / (gw.GRID_HEIGHT-1)])  # Normalisé
 
 if st.sidebar.button("Code movement"):
     for episode in range(n_episodes):
@@ -51,28 +87,28 @@ if st.sidebar.button("Code movement"):
             x, y = st.session_state.agent_pos
             # st.session_state.Q[x][y][a] 
             # a : 0=up, 1=down, 2=left, 3=right
-            dict_actions = {
-                                0:'up', 
-                                1:'down', 
-                                2:'left', 
-                                3:'right'
-                            }
-
             
             # On “suspend” le script pour 2 secondes pour donner le temps de voir la position
             time.sleep(sleep_time)
             
             Q_a_s = st.session_state.Q[x][y]
 
-            actions = epsilon_greedy_choice(Q_a_s, epsilon)
+            action = epsilon_greedy_choice(Q_a_s, epsilon)
 
-            if agent.move_agent(dict_actions[actions]):
+            if agent.move_agent(dict_actions[action]):
                 x_next, y_next = st.session_state.agent_pos
 
                 Q_a_s_next = st.session_state.Q[x_next][y_next]
 
                 # Q(s,a) ← Q(s,a) + α[r + γmaxQ(s′,a′) − Q(s,a)] (à compléter)
-                st.session_state.Q[x][y][actions] = Q_a_s[actions] + alpha * (st.session_state.R[x_next][y_next] \
-                                                                            + gama * max(Q_a_s_next) - Q_a_s[actions])
+                st.session_state.Q[x][y][action] = Q_a_s[action] + alpha * (st.session_state.R[x_next][y_next] \
+                                                                            + gama * max(Q_a_s_next, key=abs) - Q_a_s[action])
 
                 gw.dessiner_grille(placeholder)
+
+
+
+if st.sidebar.button("Entraîner Deep Q-Learning"):
+    model = build_model(input_dim=2, output_dim=4)
+    model = train_dqn(model, gw, agent, placeholder, epsilon, gama,alpha, episodes=10)
+    model.save("dqn_model.h5")
--- training.py
+++ training.py
@@ -1,4 +1,8 @@
 import random
+from tensorflow import keras
+import numpy as np
+import pandas as pd
+import streamlit as st
 
 def epsilon_greedy_choice(Q_s, epsilon):
     """
@@ -25,3 +29,72 @@ def epsilon_greedy_choice(Q_s, epsilon):
 
 
 # Q(s,a) ← Q(s,a) + α[r + γmaxQ(s′,a′) − Q(s,a)] (à compléter)
+
+def build_model(input_dim, output_dim):
+    model = keras.Sequential([
+        keras.layers.Dense(24, activation='relu', input_shape=(input_dim,)),
+        keras.layers.Dense(24, activation='relu'),
+        keras.layers.Dense(output_dim, activation='linear')
+    ])
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss='mse')
+    return model
+
+
+def get_normalise_state(x, y, gw):
+    return np.array([x / (gw.GRID_WIDTH-1), y / (gw.GRID_HEIGHT-1)]).reshape(1,-1)  # Normalisé
+
+def train_dqn(model, gw, agent, placeholder, epsilon,gama,alpha, episodes=10):
+    dict_actions = {
+                0:'up', 
+                1:'down', 
+                2:'left', 
+                3:'right'
+            }
+    
+    for episode in range(episodes):
+        target = list()
+        X = list()
+
+        gw.reset_position( placeholder)
+        while st.session_state.agent_pos != gw.goal_pos :
+            x, y = st.session_state.agent_pos
+            state = get_normalise_state(x, y, gw)
+
+            u = random.random()
+
+            # Si on explore...
+            if u < epsilon:
+                # Choix aléatoire parmi toutes les actions
+                action = random.randint(0, len(dict_actions) - 1)
+            else:
+                q_pred = model.predict(state, verbose=0)[0]
+                action = np.argmax(q_pred)
+            
+            if agent.move_agent(dict_actions[action]):
+                x_next, y_next = st.session_state.agent_pos
+                next_state = get_normalise_state(x_next, y_next, gw)
+                q_next = model.predict(next_state, verbose=0)[0]
+
+                reward = st.session_state.R[x_next][y_next]
+
+                target_value = reward + gama * np.max(q_next)
+                q_pred[action] = (1 - alpha) * q_pred[action] + alpha * target_value
+
+                st.session_state.Q[x][y] = q_pred
+                gw.dessiner_grille(placeholder)
+
+                X.append(state.flatten())
+                target.append(q_pred)
+
+                df = pd.DataFrame(X, columns=['x', 'y'])
+                df_target = pd.DataFrame(target, columns=['q_up', 'q_down', 'q_left', 'q_right'])
+                df = pd.concat([df, df_target], axis=1)
+                df.to_csv("data.csv", index=False)
+
+    
+        model.fit( df[['x', 'y']], df[['q_up', 'q_down', 'q_left', 'q_right']], 
+                  batch_size=32, shuffle=True,  # epochs=10, verbose=0
+                  validation_split=0.2,  # Pour valider le modèle
+            epochs=10, verbose=0)
+        
+    return model
